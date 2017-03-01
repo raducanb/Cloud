e20b811b83df015402ba2f21648ad29f6d691134
Bogdan Raducan <bogdan.raducan@emag.ro>
2017-03-01 20:20:49 +0200

    Add interface so the user cand input the location name thourgh from with GET request

--- T1/app.js
+++ T1/app.js
@@ -1,41 +1,46 @@
-var locationName = 'Munchen';
-
 var coordinatesService = require('./coordinatesService.js');
 var flickrService = require('./flickrService.js');
 var foursquareService = require('./foursquareService.js');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var locationData = {};
 
-coordinatesService.getCoordinates(locationName,
-  function(coordinates){
-    console.log("Coordinates of " + locationName + ": " + coordinates);
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/photos', function (req, res) {
+  locationName = req.query.location;
+  coordinatesService.getCoordinates(locationName,
+    function (coordinates) {
+      var lat = coordinates[0];
+      var lon = coordinates[1];
 
-    var lat = coordinates[0];
-    var lon = coordinates[1];
+      var locationCoordinates = {};
+      locationCoordinates.lat = parseFloat(lat).toFixed(3);
+      locationCoordinates.lon = parseFloat(lon).toFixed(3);
 
-    var locationCoordinates = {};
-    locationCoordinates.lat = parseFloat(lat).toFixed(3);
-    locationCoordinates.lon = parseFloat(lon).toFixed(3);
+      locationData.coordinates = locationCoordinates;
 
-    locationData.coordinates = locationCoordinates;
-
-    foursquareService.getNameForMostPopularVenue(locationCoordinates,
-      function(venueName){
-        console.log("Most popular venue in " + locationName + ": " + venueName);
-
-        locationData.mostPopularVenueName = venueName;
-
-        getPhotosForLocationData(locationData);
-      });
+      foursquareService.getNameForMostPopularVenue(locationCoordinates,
+        function (venueName) {
+          locationData.mostPopularVenueName = venueName;
+          getPhotosForLocationData(locationData);
+        });
     }
   );
 
-var getPhotosForLocationData = function(locationData) {
-  flickrService.getPhotos(
-    locationData.coordinates,
-    locationData.mostPopularVenueName,
-    function(photosURLsArray){
-      console.log("Photos of venue: ");
-      console.log(photosURLsArray);
-    });
+  var getPhotosForLocationData = function (locationData) {
+    flickrService.getPhotos(
+      locationData.coordinates,
+      locationData.mostPopularVenueName,
+      function (photosURLsArray) {
+        locationData.photosURLsArray = photosURLsArray;
+      });
   };
+
+
+    }
+  }
+});
+
+app.listen(8080, function () { });
