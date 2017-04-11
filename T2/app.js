e9ba38d8d3829972e151bb47a51ae18f885f0d6b
Bogdan Raducan <bogdan.raducan@emag.ro>
2017-04-11 22:13:40 +0300

    Remove ‘express’ library and only use ‘url’ and ‘http’

--- T2/app.js
+++ T2/app.js
@@ -1,22 +1,21 @@
-var express = require('express'),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+var mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    url = require('url');
+var bookRoutes = require('./routes/bookRoutes.js');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
-var Book = require('./models/bookModel');
+var port = process.env.PORT || 1338;
 
-var app = express();
-
-var port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-
-bookRouter = require('./routes/bookRoutes')(Book);
-
-app.use('/api/books', bookRouter);
-
-app.listen(port, function(){
+var serv = http.createServer(function (req, res) {
+  var purl = url.parse(req.url, true);
+  if (!purl.pathname.startsWith('/api/books')) {
+    res.writeHead(404, {'Content-Type': 'text/plain; charset=UTF-8'});
+    res.end({error: "Not found"});
+  }
+  bookRoutes.handleRouteCall(req, res);
+});
+serv.listen(port, function(){
   console.log("listening on port: " + port);
 });
--- T2/routes/bookRoutes.js
+++ T2/routes/bookRoutes.js
@@ -1,112 +1,226 @@
-var express = require('express');
+var url = require('url'),
+  qs = require('querystring'),
+  Book = require('../models/bookModel');
 
-var routes = function(Book){
-  var bookRouter = express.Router();
+module.exports.handleRouteCall =
+  function (req, res) {
+    var regExpBook = new RegExp("/api/books/([a-zA-Z0-9]+)/*$");
+    var purl = url.parse(req.url, true);
 
-  bookRouter.route('/')
-    .post(function(req, res){
-      var book = new Book(req.body);
-      book.save();
-
-      res.status(201).send(book);
-    })
-    .get(function(req, res){
-      var query = {}
-
-      if(req.query.genre) {
-        query.genre = req.query.genre;
-      }
-
-      Book.find(query, function(error, books){
-          if (error) {
-            res.status(500).send(error);
-          } else if (books.length > 0) {
-            var booksWithLinks = []
-
-            books.forEach(function(element, index, array){
-              var bookJson = element.toJSON();
-
-              bookJson.links = {};
-              bookJson.links.self = "http://" + req.headers.host + "/api/books/"
-                                    + bookJson._id;
-
-              booksWithLinks.push(bookJson);
-            });
-
-            res.json(booksWithLinks);
-          } else {
-            res.status(404).send({error: "No books found"
-                                  + req.query.genre ? "with genre '" + query.genre + "'" : ""});
-          }
+    if (purl.pathname == '/api/books'
+      || purl.pathname == '/api/books/') {
+      if (purl.query.genre) {
+        genrePath(req, purl.query.genre, function(response){
+        sendResponse(res, response);
         });
-      });
-
-  bookRouter.use('/:bookId', function(req, res, next){
-    Book.findById(req.params.bookId, function(error, book){
-      if (error) {
-        res.status(500).send(error);
-      } else if (book) {
-        req.book = book;
-        next();
-      } else {
-        res.status(404).send("Book not found.");
       }
+      simplePath(req, function (response) {
+        sendResponse(res, response);
+      });
+    } else if (regExpBook.test(purl.pathname)) {
+      var match = regExpBook.exec(purl.pathname);
+      bookPath(req, match[1], function (response) {
+        sendResponse(res, response);
+      })
+    }
+  };
+
+var sendResponse = function (res, response) {
+  res.writeHead(response.code);
+  res.end(JSON.stringify(response.data, null, ' '))
+}
+
+var simplePath = function (req, callback) {
+  if (req.method == 'GET') {
+    getBooks(req.headers.host, null, callback);
+  } else if (req.method == 'POST') {
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
     });
-  });
-  bookRouter.route('/:bookId')
-      .get(function(req, res){
-        var bookJson = req.book.toJSON();
-
-        var filterByThisGenreLink = "http://" + req.headers.host + "/api/books?genre="
-                              + bookJson.genre;
-
-        bookJson.links = {};
-        bookJson.links.FilterByThisGenre = filterByThisGenreLink.replace(" ", "%20");
-
-        res.json(bookJson);
-      })
-      .put(function(req, res){
-        req.book.title = req.body.title;
-        req.book.author = req.body.author;
-        req.book.genre = req.body.genre;
-        req.book.read = req.body.read;
-
-        req.book.save(function(error){
-          if (error) {
-            res.status(404).send(error);
-          } else {
-            res.json(req.book);
-          }
-        });
-      })
-      .patch(function(req, res){
-        if (req.body._id) {
-          delete req.body._id;
-        }
-
-        for(var key in req.body) {
-          req.book[key] = req.body[key];
-        }
-
-        req.book.save(function(error){
-          if (error) {
-            res.status(404).send(error);
-          } else {
-            res.json(req.book);
-          }
-        });
-      })
-      .delete(function(req, res){
-        req.book.remove(function(error){
-          if (error) {
-            res.status(500).send(error);
-          } else {
-            res.status(204).send("Removed");
-          }
-        });
+    req.on('end', function () {
+      var post = qs.parse(body);
+      if (post.__v) {
+        delete post.__v;
+      }
+      var book = new Book(post);
+      book.save();
+      callback({
+        code: 201,
+        data: book
       });
-
-  return bookRouter;
+    });
+  }
 };
 
-module.exports = routes;
+var bookPath = function (req, bookId, callback) {
+  if (req.method == 'GET') {
+    Book.findById(bookId, function (error, book) {
+      if (error) {
+        callback({
+          code: 500,
+          data: error
+        });
+      } else if (book) {
+        var bookJson = book.toJSON();
+        var filterByThisGenreLink = "http://" + req.headers.host + "/api/books?genre="
+          + bookJson.genre;
+        bookJson.links = {};
+        bookJson.links.FilterByThisGenre = filterByThisGenreLink.replace(" ", "%20");
+        callback({
+          code: 200,
+          data: bookJson
+        })
+      } else {
+        callback({
+          code: 404,
+          data: "Book not found"
+        });
+      }
+    });
+  } else if (req.method == 'PUT') {
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
+    });
+    req.on('end', function () {
+      var post = qs.parse(body);
+      Book.findById(bookId, function (error, book) {
+        if (error) {
+          callback({
+            code: 500,
+            data: error
+          });
+        } else if (book) {
+          book.title = post.title;
+          book.author = post.author;
+          book.genre = post.genre;
+          book.read = post.read;
+          book.save(function (error) {
+            if (error) {
+              callback({
+                code: 404,
+                data: error
+              })
+            } else {
+              callback({
+                code: 200,
+                data: book.toJSON()
+              })
+            }
+          });
+        } else {
+          callback({
+            code: 404,
+            data: "Book not found"
+          });
+        }
+      });
+    });
+  } else if (req.method == 'PATCH') {
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
+    });
+    req.on('end', function () {
+      var post = qs.parse(body);
+      Book.findById(bookId, function (error, book) {
+        if (error) {
+          callback({
+            code: 500,
+            data: error
+          });
+        } else if (book) {
+          if (post._id) {
+            delete post._id;
+          }
+          for (var key in post) {
+            book[key] = post[key];
+          }
+          book.save(function (error) {
+            if (error) {
+              callback({
+                code: 404,
+                data: error
+              })
+            } else {
+              callback({
+                code: 200,
+                data: book.toJSON()
+              })
+            }
+          });
+        }
+      })
+    });
+  } else if (req.method == 'DELETE') {
+    Book.findById(bookId, function (error, book) {
+      if (error) {
+        callback({
+          code: 500,
+          data: error
+        });
+      } else if (book) {
+        book.remove(function (error) {
+          if (error) {
+            callback({
+              code: 500,
+              data: error
+            });
+          } else {
+            callback({
+              code: 204,
+              data: "Removed"
+            });
+          }
+        })
+      }
+    });
+  }
+}
+
+var genrePath = function (req, genre, callback) {
+  if (req.method == 'GET') {
+    getBooks(req.headers.host, genre, callback);
+  }
+}
+
+var getBooks = function (host, genre, callback) {
+  var query = {}
+  if (genre) {
+    query.genre = genre;
+  }
+
+  Book.find(query, function (error, books) {
+    if (error) {
+      callback({
+        code: 500,
+        data: error
+      });
+    } else if (books.length > 0) {
+      var booksWithLinks = []
+
+      books.forEach(function (element, index, array) {
+        var bookJson = element.toJSON();
+
+        bookJson.links = {};
+        bookJson.links.self = "http://" + host + "/api/books/"
+          + bookJson._id;
+
+        booksWithLinks.push(bookJson);
+      });
+      callback({
+        code: 200,
+        data: booksWithLinks
+      });
+    } else {
+      callback({
+        code: 404,
+        data: {
+          error: "No books found"
+            + req.query.genre ? "with genre '" + query.genre + "'" : ""
+        }
+      });
+    }
+  });
+}
